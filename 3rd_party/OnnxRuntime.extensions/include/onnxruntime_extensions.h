cbc7ca10962e2ce81ec645f753a4fd04fbc3579b
THISISAGOODNAME <yangyj19931231@gmail.com>
2024-02-04 18:10:01 +0800

    StableDiffusion 1.5 prerequests

---
+++ 3rd_party/OnnxRuntime.extensions/include/onnxruntime_extensions.h
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+// Note: The following include path is used for building Swift Package Manager support for ORT Extensions.
+// The macro is defined in cxxSettings config in Package.swift.
+// The reason why we need a prefix is that when Xcode includes the package it copies it to an internally generated path with 
+// the package name as a prefix. 
+// And we don't have control over the include paths when that happens in the user's iOS app. 
+// The only way we found to make the include path work automatically for now.
+#ifdef ORT_SWIFT_PACKAGE_MANAGER_BUILD
+#include "onnxruntime/onnxruntime_c_api.h"
+#else
+#include "onnxruntime_c_api.h"
+#endif
+
+#ifdef _WIN32
+#define ORTX_EXPORT __declspec(dllexport)
+#else
+#define ORTX_EXPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ORTX_EXPORT OrtStatus* ORT_API_CALL RegisterCustomOps(OrtSessionOptions* options, const OrtApiBase* api);
+
+#ifdef __cplusplus
+}
+#endif
